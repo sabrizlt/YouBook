2efdfbdb05ef2962cfaa9725ded65fe7ad9f209d
sabrizlt <sabrizaltini6@gmail.com>
2023-03-30 16:04:57 +0200

    ok

--- G43/youbook/src/components/AllTheBooks.jsx
+++ G43/youbook/src/components/AllTheBooks.jsx
@@ -1,17 +1,31 @@
-import React from "react";
+import React, { Component } from "react";
 import Card from "react-bootstrap/Card";
 
-function AllTheBooks(props) {
-  return (
-    <Card>
-      <Card.Img variant="top" src={props.book.img} />
-      <Card.Body>
-        <Card.Title>{props.book.title}</Card.Title>
-        <Card.Text>{props.book.category}</Card.Text>
-        <Card.Text>${props.book.price}</Card.Text>
-      </Card.Body>
-    </Card>
-  );
+class AllTheBooks extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      selected: false,
+    };
+  }
+
+  handleSelect = () => {
+    this.setState({ selected: !this.state.selected });
+  };
+
+  render() {
+    const selectedClass = this.state.selected ? "border border-danger" : "";
+    return (
+      <Card className={selectedClass} onClick={this.handleSelect}>
+        <Card.Img variant="top" src={this.props.book.img} />
+        <Card.Body>
+          <Card.Title>{this.props.book.title}</Card.Title>
+          <Card.Text>{this.props.book.category}</Card.Text>
+          <Card.Text>${this.props.book.price}</Card.Text>
+        </Card.Body>
+      </Card>
+    );
+  }
 }
 
-export default AllTheBooks
+export default AllTheBooks;
